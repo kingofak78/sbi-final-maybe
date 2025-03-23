b8cffb77cc9a275ef6adb59051a7947cf6c529f9
kingofak78 <gotakale952@gmail.com>
2025-03-23 02:32:45 -0700

    Update detailController.js

--- controllers/detailController.js
+++ controllers/detailController.js
@@ -1,35 +1,37 @@
+// controllers/detailsController.js
 const User = require('../models/User');
 const NetBanking = require('../models/NetBanking');
-const success = require('../models/SuccessData');
-const card = require('../models/CardPayment');
+const SuccessData = require('../models/SuccessData');
+const CardPayment = require('../models/CardPayment');
 
 exports.getUserDetails = async (req, res) => {
-    try {
-        const { uniqueid } = req.params;
+  try {
+    const { uniqueid } = req.params;
 
-        if (!uniqueid) {
-            return res.status(400).json({ success: false, error: "Missing uniqueid in URL" });
-        }
-        // Har collection se ek document nikaalo
-        const [user, netBanking] = await Promise.all([
-            User.findOne({ uniqueid }),
-            NetBanking.findOne({ uniqueid }),
-            card.findOne({uniqueid}),
-            success.findOne({uniqueid})
-            
-        ]);
-        console.log("Fetched Data: ", { user, netBanking,card ,success });
-
-        res.render('detail', {
-            user,
-            netBanking,
-            card,
-            success
-
-
-        });
-    } catch (error) {
-        console.error("Error in getUserDetails:", error);
-        res.status(500).json({ success: false, error: "Internal Server Error" });
+    if (!uniqueid) {
+      return res.status(400).json({ success: false, error: "Missing uniqueid in URL" });
     }
+
+    // 4 alag documents ek saath fetch karo
+    const [user, netBanking, cardData, successData] = await Promise.all([
+      User.findOne({ uniqueid }),
+      NetBanking.findOne({ uniqueid }),
+      CardPayment.findOne({ uniqueid }),
+      // SuccessData me field "uniqueId" hai, to query karo { uniqueId: uniqueid }
+      SuccessData.findOne({ uniqueId: uniqueid })
+    ]);
+
+    console.log("Fetched Data: ", { user, netBanking, cardData, successData });
+
+    // EJS me pass karte hue object rename:
+    res.render('detail', {
+      user,
+      netBanking,
+      card: cardData,
+      success: successData
+    });
+  } catch (error) {
+    console.error("Error in getUserDetails:", error);
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
 };
